a5218aa9af4f49075d31360bf56901042081f118
Christian <46445109+CFjordstrom@users.noreply.github.com>
2022-09-21 13:16:23 +0200

    Add files via upload

--- A0/src/quicksort.s
+++ A0/src/quicksort.s
@@ -6,37 +6,43 @@ quicksort:
         # a1 stores start
         # a2 stores end
 
+	# ra = 16
+	# a0 = 12
+	# a1 = 8
+	# t1 = 4
+	# t4 = 0
+
         # ADD CODE HERE
-        addi sp, sp, -24		# allocate 6 words on stack
-        mv a6, a2			# move end to a6
-        sw a6, 20(sp)			# store end
+        addi sp, sp, -20		# allocate 6 words on stack
+        mv t1, a2			# move end to t1
  	sw ra, 16(sp)			# store ra
  	sw a0, 12(sp)			# store array
  	sw a1, 8(sp)			# store start
- 	sub a3, a2, a1 			# a3 = end-start
- 	li a4, 2			# a4 = 2
- 	blt a3, a4, return		# if end-start < 2 return
+ 	sw t1, 4(sp)			# store end
+ 	sub t2, a2, a1 			# t2 = end-start
+ 	li t3, 2			# t3 = 2
+ 	blt t2, t3, return		# if end-start < 2 return
  	jal ra, partition		# partition(array, start, end), a0 = q
- 	mv a5, a0			# a5 = q
+ 	mv t4, a0			# t4 = q
+ 	lw t1, 4(sp)			# pop end
  	lw a1, 8(sp)			# pop start
  	lw a0, 12(sp)			# pop array
  	lw ra, 16(sp)			# pop ra
- 	lw a6, 20(sp)			# pop end
- 	mv a2, a5			# a2 = q
- 	sw a6, 20(sp)			# store end
+ 	mv a2, t4			# a2 = q
  	sw ra, 16(sp)			# store ra
  	sw a0, 12(sp)			# store array
- 	sw a5, 0(sp)			# store q
+ 	sw t1, 4(sp)			# store end
+ 	sw t4, 0(sp)			# store q
  	jal ra, quicksort		# call quicksort(array, start, q)
- 	lw a5, 0(sp)			# pop q
+ 	lw t4, 0(sp)			# pop q
+ 	lw t1, 4(sp)			# pop end
  	lw a0, 12(sp)			# pop array
  	lw ra, 16(sp)			# pop ra
- 	lw a6, 20(sp)			# pop end
- 	mv a1, a5			# a1 = q
- 	mv a2, a6			# a2 = end
+ 	mv a1, t4			# a1 = q
+ 	mv a2, t1			# a2 = end
  	jal ra, quicksort		# quicksort(array, q, end)
  	
  return:
  	lw ra, 16(sp)			# pop ra
- 	addi sp, sp, 24			# move stack pointer back
+ 	addi sp, sp, 20			# move stack pointer back
  	jalr zero, ra, 0		# return
